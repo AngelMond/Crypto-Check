1f3d359125857bd37c392a092f0aaec82d5ffbd9
Civic <abraham_will@hotmail.com>
2022-04-19 19:57:05 -0500

    Updating html

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+.then(res => res.json())
+.then(data =>console.log(data))
