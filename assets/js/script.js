c42a2b6566ab0d6f9ad2703dc1797b2ffbf62227
Civic <abraham_will@hotmail.com>
2022-04-20 21:14:21 -0500

    uploading js code for test's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+// fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+// .then(res => res.json())
+// .then(data =>console.log(data))
+
+
+
+// let crypto = $('.crypto');
+
+
+const aplicacion = document.querySelector('.crypto')
+
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
 .then(res => res.json())
-.then(data =>console.log(data))
+.then(data => {
+    data.forEach(token => {
+        console.log(token.name)
+
+        
+        const p = document.createElement('p')
+        p.innerHTML = token.name
+        aplicacion.appendChild(p)
+
+    });
+    //console.log(data)
+})
+
+.catch(err => console.log(err))
