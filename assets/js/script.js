8f5b32022be54d41340d9636984137bd9aaec308
AngelMond <abraham_will@hotmail.com>
2022-04-21 16:27:26 -0500

    Comments added in js and added div tag in html to store the added cryptos by user

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,13 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
 .then(res => res.json())
 .then(data =>console.log(data))
 
-
-
-
-
-let currentPrice = $('#currentPrice');
-
+//Select Tag to append option tag's
 const selectCrypto = $('#selectCrypto');
 
+//Select tag to display the current crypto value
+let currentPrice = $('#currentPrice');
+
+//Select every option tag 
 let optionTag = document.getElementsByTagName('option');
 
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -18,6 +17,8 @@ fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=mark
 
     data.forEach(token => {
         var option = $('<option>' + token.name +'</option>');
+
+        //Append option tag with every crypto name
         selectCrypto.append(option);
     });
     //console.log(data)
