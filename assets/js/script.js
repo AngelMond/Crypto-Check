e8fc91e2cdbc2450f11f89c4fcfb6554bb01c75b
macosta2015 <macosta2015@my.fit.edu>
2022-04-28 19:55:47 -0400

    API is running
    The API is running under the function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,120 @@
-//Select Tag to append option tag's
-const selectCrypto = $('#selectCrypto');
-const selectCryptoAlert = $('#selectCryptoAlert');
-
-//Select tag to display the current crypto value
-let currentPrice = $('#currentPrice');
-var valores = 0;
-
-//Select the input to enter the user amount
-let userAmount = $('#userAmount');
-
-//Select the modal
-let modalForInvalidNumber = $('#defaultModal')
-
-//Select span to append the invest value
-let investValue = $('#investValue');
-
-//Select span to append the mexican pesos 
-let mexicanPesos = $('#currentPriceMxn');
-
-//Button to close the modal
-let modalButton = $('#modalButton');
-
-
-
-userAmount.on('keyup', () => {
-
-        if (userAmount.val() >= 0){
-
-        var prueba = userAmount.val();
-        investValue.html('$');
-        investValue.append(prueba*valores);
-
-    } else {
-
-        modalForInvalidNumber.removeClass('hidden');
-        modalButton.on('click', ()=>{
-            modalForInvalidNumber.addClass('hidden');
-            userAmount.val('');
-        })
-        }
+               //Code for converting the Mexican pesos to US Dollars
+        const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Host': 'currency-converter-by-api-ninjas.p.rapidapi.com',
+                    'X-RapidAPI-Key': '6c37f3f275msh7913bbd983d34f5p1e0db5jsn3a74ed99c007'
+                }
+            };
     
-})
+            async function getCurrency() {
+                return await fetch(
+                    "https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency?have=USD&want=MXN&amount=1",
+                    options
+                );
+            }
 
 
-//This function is when the user select a crypto from the select tag
-function displayValue (){
+var exchangeValue = getCurrency()
+.then((response) => {
+    return response.json();
+            })
+            .then((data) => {
+                console.log('inside the getCurrency()')
+                console.log(data.new_amount);
 
-    selectCrypto.change(function(){
-        userAmount.val('');
-        investValue.html('$0.00')
-        valores = selectCrypto.val();
-        currentPrice.html('$');
-        currentPrice.append(valores);
-    });
-};
+                mexicanPesosToDollars = data.new_amount;
+                console.log('Hola: ' + mexicanPesosToDollars)
+
+       //Select Tag to append option tag's
+        const selectCrypto = $('#selectCrypto');
+        const selectCryptoAlert = $('#selectCryptoAlert');
+
+        //Select tag to display the current crypto value
+        let currentPrice = $('#currentPrice');
+        var valores = 0;
+
+        //Select the input to enter the user amount
+        let userAmount = $('#userAmount');
+
+        //Select the modal
+        let modalForInvalidNumber = $('#defaultModal')
+
+        //Select span to append the invest value
+        let investValue = $('#investValue');
+
+        //Select span to append the mexican pesos 
+        let mexicanPesos = $('#currentPriceMxn');
+        var valoredPesosDolares = mexicanPesosToDollars;
+        mexicanPesos.append(valoredPesosDolares);
 
 
-fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-.then(res => res.json())
+        //Button to close the modal
+        let modalButton = $('#modalButton');
+        userAmount.on('keyup', () => {
 
-.then(data => {
+                if (userAmount.val() >= 0){
 
-    data.forEach(token => {
-       
-        var optionElement = $('<option>');
-        optionElement.html(token.name);
-        optionElement.attr( 'value', token.current_price);
+                var prueba = userAmount.val();
+                investValue.html('$');
+                investValue.append(prueba*valores);
+
+            } else {
+
+                modalForInvalidNumber.removeClass('hidden');
+                modalButton.on('click', ()=>{
+                    modalForInvalidNumber.addClass('hidden');
+                    userAmount.val('');
+                })
+                }
+            
+        })
+
+
+        //This function is when the user select a crypto from the select tag
+        function displayValue (){
+
+            selectCrypto.change(function(){
+                userAmount.val('');
+                investValue.html('$0.00')
+                valores = selectCrypto.val();
+                currentPrice.html('$');
+                currentPrice.append(valores);
+            });
+        };
+
+
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+        .then(res => res.json())
+
+        .then(data => {
+
+            data.forEach(token => {
+            
+                var optionElement = $('<option>');
+                optionElement.html(token.name);
+                optionElement.attr( 'value', token.current_price);
+                
+                //Append option tag with every crypto name
+                selectCrypto.append(optionElement);
+            });
+
+            data.forEach(token => {
+
+                var optionElement = $('<option>');
+                optionElement.html(token.name);
+                selectCryptoAlert.append(optionElement);
+                
+
+            });
+
+            displayValue ();
         
-        //Append option tag with every crypto name
-        selectCrypto.append(optionElement);
+        })
+        .catch(err => console.log(err));
     });
 
-    data.forEach(token => {
 
-        var optionElement = $('<option>');
-        optionElement.html(token.name);
-        selectCryptoAlert.append(optionElement);
-        
-
-    });
-
-    displayValue ();
-  
-})
-.catch(err => console.log(err));
-
+console.log('Value of the exchange: ' + exchangeValue)
 
 
